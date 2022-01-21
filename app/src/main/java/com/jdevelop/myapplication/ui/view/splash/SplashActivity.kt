63e30cd048273b7f8dd1ce53ca13dc53d28a8365
Jdevelop057 <jdevelop057@gmail.com>
2022-01-21 19:12:45 +0100

    Add components SplashActivity

--- app/src/main/java/com/jdevelop/myapplication/ui/view/splash/SplashActivity.kt
+++ app/src/main/java/com/jdevelop/myapplication/ui/view/splash/SplashActivity.kt
@@ -2,10 +2,25 @@ package com.jdevelop.myapplication.ui.view.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jdevelop.myapplication.R
+import com.jdevelop.myapplication.databinding.ActivitySplashBinding
+import com.jdevelop.myapplication.ui.view.main.MainRouter
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.buttonParty.setOnClickListener {
+            MainRouter().launch(this)
+        }
+        binding.buttonInfo.setOnClickListener {
+
+        }
     }
 }
