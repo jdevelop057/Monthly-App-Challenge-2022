45af5c8f9ce5f7137355d909a2698b8a0b71c95e
Jdevelop057 <jdevelop057@gmail.com>
2022-01-21 19:11:28 +0100

    Create BaseActivityRouter

--- app/src/main/java/com/jdevelop/myapplication/ui/base/BaseActivityRouter.kt
+++ app/src/main/java/com/jdevelop/myapplication/ui/base/BaseActivityRouter.kt
@@ -1,7 +1,14 @@
 package com.jdevelop.myapplication.ui.base
 
+import android.content.Context
+import android.content.Intent
+
 /**
  * Created by Jdevelop057 on 21/01/2022.
-*/
-class BaseRouter {
+ */
+
+
+interface BaseActivityRouter {
+    fun intent(activity: Context): Intent
+    fun launch(activity: Context) = activity.startActivity(intent(activity))
 }
