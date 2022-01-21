4aff7bce9e7b5b0824fb06c28cd188a46d06b3da
Jdevelop057 <jdevelop057@gmail.com>
2022-01-21 19:11:54 +0100

    Create MainRouter with BaseActivityRouter

--- app/src/main/java/com/jdevelop/myapplication/ui/view/main/MainRouter.kt
+++ app/src/main/java/com/jdevelop/myapplication/ui/view/main/MainRouter.kt
@@ -1,7 +1,12 @@
 package com.jdevelop.myapplication.ui.view.main
 
+import android.content.Context
+import android.content.Intent
+import com.jdevelop.myapplication.ui.base.BaseActivityRouter
+
 /**
  * Created by Jdevelop057 on 21/01/2022.
-*/
-class MainRouter {
+ */
+class MainRouter: BaseActivityRouter {
+    override fun intent(activity: Context): Intent = Intent(activity, MainActivity::class.java)
 }
