aaa7c9fc7cfeb9ac643ddfc84dd3802fd4f1ea35
Jdevelop057 <jdevelop057@gmail.com>
2022-01-21 19:13:05 +0100

    Create InfoActivity

--- app/src/main/java/com/jdevelop/myapplication/ui/view/info/InfoActivity.kt
+++ app/src/main/java/com/jdevelop/myapplication/ui/view/info/InfoActivity.kt
@@ -2,6 +2,7 @@ package com.jdevelop.myapplication.ui.view.info
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jdevelop.myapplication.R
 
 class InfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
