598f63244d220ef2ce82b5481be32cb13eff1cf0
Jdevelop057 <jdevelop057@gmail.com>
2022-01-21 19:02:13 +0100

    Add viewBinding

---
+++ app/src/main/java/com/jdevelop/myapplication/ui/base/BaseActivityRouter.kt
@@ -0,0 +1,7 @@
+package com.jdevelop.myapplication.ui.base
+
+/**
+ * Created by Jdevelop057 on 21/01/2022.
+*/
+class BaseRouter {
+}
---
+++ app/src/main/java/com/jdevelop/myapplication/ui/view/main/MainRouter.kt
@@ -0,0 +1,7 @@
+package com.jdevelop.myapplication.ui.view.main
+
+/**
+ * Created by Jdevelop057 on 21/01/2022.
+*/
+class MainRouter {
+}
