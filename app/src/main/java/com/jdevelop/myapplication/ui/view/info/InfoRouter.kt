d23c4f10c1d6d16c7bc1ab62f52f140b8b9f99dd
Jdevelop057 <jdevelop057@gmail.com>
2022-01-22 01:42:48 +0100

    Create InfoRouter

--- app/src/main/java/com/jdevelop/myapplication/ui/view/info/InfoRouter.kt
+++ app/src/main/java/com/jdevelop/myapplication/ui/view/info/InfoRouter.kt
@@ -1,7 +1,12 @@
 package com.jdevelop.myapplication.ui.view.info
 
+import android.content.Context
+import android.content.Intent
+import com.jdevelop.myapplication.ui.base.BaseActivityRouter
+
 /**
  * Created by Jdevelop057 on 22/01/2022.
-*/
-class InfoRouter {
+ */
+class InfoRouter: BaseActivityRouter {
+    override fun intent(activity: Context): Intent = Intent(activity, InfoActivity::class.java)
 }
