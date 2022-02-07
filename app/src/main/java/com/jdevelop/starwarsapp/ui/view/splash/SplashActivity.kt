908ebc0c9680a8e894dfc3662ea236def0ce7dbb
Jdevelop057 <jdevelop057@gmail.com>
2022-02-07 21:05:12 +0100

    Create SplashActivity

---
+++ app/src/main/java/com/jdevelop/starwarsapp/ui/view/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.jdevelop.starwarsapp.ui.view.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jdevelop.starwarsapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
