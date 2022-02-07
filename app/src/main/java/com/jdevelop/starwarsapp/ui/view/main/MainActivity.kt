6e62a2d28c6999b819139e6da9bc1361cd732bb3
Jdevelop057 <jdevelop057@gmail.com>
2022-02-07 20:33:42 +0100

    Add id parent ConstraintLayout

--- app/src/main/java/com/jdevelop/starwarsapp/ui/view/main/MainActivity.kt
+++ app/src/main/java/com/jdevelop/starwarsapp/ui/view/main/MainActivity.kt
@@ -2,14 +2,21 @@ package com.jdevelop.starwarsapp.ui.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.jdevelop.starwarsapp.R
 import com.jdevelop.starwarsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var parentConstraintLayout: ConstraintLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setup()
+    }
+
+    private fun setup() {
+        parentConstraintLayout = binding.parentConstraintLayout
     }
 }
