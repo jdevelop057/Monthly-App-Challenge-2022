cfdd8f3e096e7a57f77e219a94aacf0f1b67074b
Jdevelop057 <jdevelop057@gmail.com>
2022-02-07 20:23:05 +0100

    Create ui.view packages

--- app/src/main/java/com/jdevelop/starwarsapp/ui/view/main/MainActivity.kt
+++ app/src/main/java/com/jdevelop/starwarsapp/ui/view/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jdevelop.starwarsapp
+package com.jdevelop.starwarsapp.ui.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jdevelop.starwarsapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
