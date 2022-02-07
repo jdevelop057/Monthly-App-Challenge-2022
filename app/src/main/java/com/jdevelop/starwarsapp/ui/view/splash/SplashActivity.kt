3f81bab507cb63d50356a6250e18dd3f0bb74f9f
Jdevelop057 <jdevelop057@gmail.com>
2022-02-07 22:48:48 +0100

    Add ComponentProgressBar to Splash Activity Kotlin

--- app/src/main/java/com/jdevelop/starwarsapp/ui/view/splash/SplashActivity.kt
+++ app/src/main/java/com/jdevelop/starwarsapp/ui/view/splash/SplashActivity.kt
@@ -1,15 +1,23 @@
 package com.jdevelop.starwarsapp.ui.view.splash
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import com.jdevelop.starwarsapp.R
+import androidx.appcompat.app.AppCompatActivity
 import com.jdevelop.starwarsapp.databinding.ActivitySplashBinding
+import com.jdevelop.starwarsapp.ui.component.progressbar.ComponentProgressBar
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
+    private lateinit var componentProgressBar: ComponentProgressBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setup()
+    }
+
+    private fun setup() {
+        componentProgressBar = binding.componentProgressBar
+        componentProgressBar.setProgressBarColor(Color.CYAN)
     }
 }
