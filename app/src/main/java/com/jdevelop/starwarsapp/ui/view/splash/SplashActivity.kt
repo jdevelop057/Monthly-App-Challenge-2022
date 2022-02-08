ac09291a5d942d4736653e2af04cf7f425563057
Jdevelop057 <jdevelop057@gmail.com>
2022-02-08 23:39:41 +0100

    Add SplashViewModel to SplashActivity

--- app/src/main/java/com/jdevelop/starwarsapp/ui/view/splash/SplashActivity.kt
+++ app/src/main/java/com/jdevelop/starwarsapp/ui/view/splash/SplashActivity.kt
@@ -3,12 +3,15 @@ package com.jdevelop.starwarsapp.ui.view.splash
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.jdevelop.starwarsapp.databinding.ActivitySplashBinding
 import com.jdevelop.starwarsapp.ui.component.progressbar.ComponentProgressBar
+import com.jdevelop.starwarsapp.ui.viewmodel.splash.SplashViewModel
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
     private lateinit var componentProgressBar: ComponentProgressBar
+    private lateinit var viewModel: SplashViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
@@ -19,5 +22,6 @@ class SplashActivity : AppCompatActivity() {
     private fun setup() {
         componentProgressBar = binding.componentProgressBar
         componentProgressBar.setProgressBarColor(Color.CYAN)
+        viewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
     }
 }
