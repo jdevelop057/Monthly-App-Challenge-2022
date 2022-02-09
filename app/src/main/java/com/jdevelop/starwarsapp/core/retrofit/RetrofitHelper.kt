d147bd9cbe7b03a925d2c93d1c879ceeffa06201
Jdevelop057 <jdevelop057@gmail.com>
2022-02-09 23:14:38 +0100

    Add logic to object RetrofitHelper

--- app/src/main/java/com/jdevelop/starwarsapp/core/retrofit/RetrofitHelper.kt
+++ app/src/main/java/com/jdevelop/starwarsapp/core/retrofit/RetrofitHelper.kt
@@ -1,4 +1,17 @@
 package com.jdevelop.starwarsapp.core.retrofit
 
+/**
+ * Created by Jdevelop057 on 09/02/2022.
+ */
+import com.jdevelop.starwarsapp.util.Constants
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitHelper {
+    fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.STAR_WARS_API)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
