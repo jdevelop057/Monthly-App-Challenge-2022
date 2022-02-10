0f1d798d80bc1aaa5cb0381ac8fd310d3b7727e9
Jdevelop057 <jdevelop057@gmail.com>
2022-02-10 22:45:12 +0100

    Add suspend fun getPeople to PeopleApiService

--- app/src/main/java/com/jdevelop/starwarsapp/data/network/people/PeopleApiService.kt
+++ app/src/main/java/com/jdevelop/starwarsapp/data/network/people/PeopleApiService.kt
@@ -1,7 +1,22 @@
 package com.jdevelop.starwarsapp.data.network.people
 
+import com.jdevelop.starwarsapp.core.retrofit.RetrofitHelper
+import com.jdevelop.starwarsapp.data.model.people.PeopleModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+
 /**
  * Created by Jdevelop057 on 10/02/2022.
  */
 class PeopleApiService {
+
+    private val retrofit: Retrofit = RetrofitHelper.getRetrofit()
+
+    suspend fun getPeople(): List<PeopleModel> {
+        return withContext(Dispatchers.IO) {
+            val response = retrofit.create(PeopleApiClient::class.java).getAllPeople()
+            response.body() ?: emptyList()
+        }
+    }
 }
