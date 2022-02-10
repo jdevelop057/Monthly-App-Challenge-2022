a96034889bbb5a7873de383aee40874894dd1ab5
Jdevelop057 <jdevelop057@gmail.com>
2022-02-10 23:17:38 +0100

    Add SplashViewModel GetAllPeople

--- app/src/main/java/com/jdevelop/starwarsapp/ui/viewmodel/splash/SplashViewModel.kt
+++ app/src/main/java/com/jdevelop/starwarsapp/ui/viewmodel/splash/SplashViewModel.kt
@@ -1,6 +1,27 @@
 package com.jdevelop.starwarsapp.ui.viewmodel.splash
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jdevelop.starwarsapp.data.model.people.PeopleModel
+import com.jdevelop.starwarsapp.domain.usecases.people.GetAllPeople
+import kotlinx.coroutines.launch
+
 /**
- * Created by Teacher Heisenberg on 08/02/2022.
+ * Created by Jdevelop057 on 08/02/2022.
  */
-class SplashViewModel {
+class SplashViewModel : ViewModel() {
+
+    var splashViewModelGetData = MutableLiveData<PeopleModel>()
+    val getAllPeople: GetAllPeople = GetAllPeople()
+
+    fun getData() {
+        viewModelScope.launch {
+            val result = getAllPeople()
+            if(result != null){
+                splashViewModelGetData.postValue(result!!)
+            }
+        }
+    }
+
 }
