d849b17086bd2da21e8540d388b1327608427e7e
Jdevelop057 <jdevelop057@gmail.com>
2022-02-10 22:46:49 +0100

    Fix typo

---
+++ app/src/main/java/com/jdevelop/starwarsapp/data/repositories/people/PeopleRepository.kt
@@ -0,0 +1,11 @@
+package com.jdevelop.starwarsapp.data.repositories.people
+
+import com.jdevelop.starwarsapp.data.network.people.PeopleApiService
+
+/**
+ * Created by Jdevelop057 on 10/02/2022.
+ */
+class PeopleRepository {
+
+    private val api = PeopleApiService()
+}
--- app/src/main/java/com/jdevelop/starwarsapp/data/repositories/people/PeopleRespository.kt
+++
@@ -1,7 +0,0 @@
-package com.jdevelop.starwarsapp.data.repositories.people
-
-/**
- * Created by Jdevelop057 on 10/02/2022.
- */
-class PeopleRespository {
-}
