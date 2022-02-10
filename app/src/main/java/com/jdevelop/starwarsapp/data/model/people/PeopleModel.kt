30cf41c445d450775d26bf869f04b18f335a5385
Jdevelop057 <jdevelop057@gmail.com>
2022-02-10 22:36:13 +0100

    Update PeopleModel data class

--- app/src/main/java/com/jdevelop/starwarsapp/data/model/people/PeopleModel.kt
+++ app/src/main/java/com/jdevelop/starwarsapp/data/model/people/PeopleModel.kt
@@ -1,6 +1,14 @@
 package com.jdevelop.starwarsapp.data.model.people
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by Jdevelop057 on 10/02/2022.
  */
-data class PeopleModel()
+
+data class PeopleModel(
+    @SerializedName("count") val count: Int,
+    @SerializedName("next") val next: String?,
+    @SerializedName("previous") val previous: String?,
+    @SerializedName("results") val results: List<PeopleResultsModel>,
+)
