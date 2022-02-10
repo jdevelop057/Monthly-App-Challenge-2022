23f0595c08e30ac896a6c71ff5549abde75ca536
Jdevelop057 <jdevelop057@gmail.com>
2022-02-10 23:11:37 +0100

    Fix: Star Wars API Url

--- app/src/main/java/com/jdevelop/starwarsapp/util/Constants.kt
+++ app/src/main/java/com/jdevelop/starwarsapp/util/Constants.kt
@@ -4,6 +4,6 @@ package com.jdevelop.starwarsapp.util
  * Created by Jdevelop057 on 09/02/2022.
  */
 object Constants {
-    const val STAR_WARS_API = "https://swapi.dev/"
+    const val STAR_WARS_API = "https://swapi.dev/api/"
     const val STAR_WARS_API_PEOPLE = STAR_WARS_API + "people/"
 }
