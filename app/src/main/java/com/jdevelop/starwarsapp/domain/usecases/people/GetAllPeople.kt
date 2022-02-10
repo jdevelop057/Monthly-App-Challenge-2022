082fa9e6b3cd27a4263d0ec05d8d1876f740b0a2
Jdevelop057 <jdevelop057@gmail.com>
2022-02-10 22:54:23 +0100

    Add logic to usecase GetAllPeople

--- app/src/main/java/com/jdevelop/starwarsapp/domain/usecases/people/GetAllPeople.kt
+++ app/src/main/java/com/jdevelop/starwarsapp/domain/usecases/people/GetAllPeople.kt
@@ -1,7 +1,15 @@
 package com.jdevelop.starwarsapp.domain.usecases.people
 
+import com.jdevelop.starwarsapp.data.model.people.PeopleModel
+import com.jdevelop.starwarsapp.data.repositories.people.PeopleRepository
+
 /**
  * Created by Jdevelop057 on 10/02/2022.
  */
 class GetAllPeople {
+
+    private val repository: PeopleRepository = PeopleRepository()
+
+    suspend operator fun invoke(): List<PeopleModel> =
+        repository.getAllPeople()
 }
