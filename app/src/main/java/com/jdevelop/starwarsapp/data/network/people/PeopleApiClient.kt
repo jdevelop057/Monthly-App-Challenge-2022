dcdb9178b9cc0a3a52d0389b6a3dbdf88c6982a2
Jdevelop057 <jdevelop057@gmail.com>
2022-02-10 22:39:24 +0100

    Add retrofit logic to PeopleApiClient (getAllPeople)

--- app/src/main/java/com/jdevelop/starwarsapp/data/network/people/PeopleApiClient.kt
+++ app/src/main/java/com/jdevelop/starwarsapp/data/network/people/PeopleApiClient.kt
@@ -1,7 +1,14 @@
 package com.jdevelop.starwarsapp.data.network.people
 
+import com.jdevelop.starwarsapp.data.model.people.PeopleModel
+import com.jdevelop.starwarsapp.util.Constants
+import retrofit2.Response
+import retrofit2.http.GET
+
 /**
  * Created by Jdevelop057 on 10/02/2022.
  */
 interface PeopleApiClient {
+    @GET(Constants.STAR_WARS_API_PEOPLE)
+    suspend fun getAllPeople(): Response<List<PeopleModel>>
 }
