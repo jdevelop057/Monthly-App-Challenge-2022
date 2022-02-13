784d7ef0bbe96502829d7c537a9795fa899281fb
Jdevelop057 <jdevelop057@gmail.com>
2022-02-13 17:57:12 +0100

    Change PeopleModel List to MutableList

--- app/src/main/java/com/jdevelop/starwarsapp/data/model/people/PeopleModel.kt
+++ app/src/main/java/com/jdevelop/starwarsapp/data/model/people/PeopleModel.kt
@@ -10,5 +10,5 @@ data class PeopleModel(
     @SerializedName("count") val count: Int,
     @SerializedName("next") val next: String?,
     @SerializedName("previous") val previous: String?,
-    @SerializedName("results") val results: List<PeopleResultsModel>,
+    @SerializedName("results") val results: MutableList<PeopleResultsModel>,
 )
