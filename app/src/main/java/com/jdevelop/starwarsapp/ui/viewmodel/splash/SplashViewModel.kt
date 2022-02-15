a77d3bda50aef31066f154bcc898703991fad287
Jdevelop057 <jdevelop057@gmail.com>
2022-02-15 23:09:37 +0100

    Clean ViewModel

--- app/src/main/java/com/jdevelop/starwarsapp/ui/viewmodel/splash/SplashViewModel.kt
+++ app/src/main/java/com/jdevelop/starwarsapp/ui/viewmodel/splash/SplashViewModel.kt
@@ -2,26 +2,17 @@ package com.jdevelop.starwarsapp.ui.viewmodel.splash
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.jdevelop.starwarsapp.data.model.people.PeopleModel
 import com.jdevelop.starwarsapp.domain.usecases.people.GetAllPeople
-import kotlinx.coroutines.launch
 
 /**
  * Created by Jdevelop057 on 08/02/2022.
  */
 class SplashViewModel : ViewModel() {
-
     var splashViewModelGetData = MutableLiveData<PeopleModel>()
     val getAllPeople: GetAllPeople = GetAllPeople()
 
-    fun getData() {
-        viewModelScope.launch {
-            val result = getAllPeople()
-            if(result != null){
-                splashViewModelGetData.postValue(result!!)
-            }
-        }
-    }
+
+    suspend fun getData(page: Int): PeopleModel? = getAllPeople(page)
 
 }
